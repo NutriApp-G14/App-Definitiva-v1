e78ae4f2655e42bbd67ad910683f428f97322513
crisdeurquia <crisvalerour@gmail.com>
2023-04-20 01:14:52 +0200

    Crear paginaRegistroComida

--- my_app/lib/views/RegistroComidas.dart
+++ my_app/lib/views/RegistroComidas.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:my_app/model/NavBar.dart';
+import 'package:my_app/model/TipoComidaCard.dart';
+
 
 class RegistroComidasPage extends StatefulWidget {
   final String nombreUsuario;
@@ -26,8 +28,16 @@ class _RegistroComidasPageState extends State<RegistroComidasPage> {
             child: NutriAppBar(nombreUsuario: widget.nombreUsuario),
           ),
         ),
-        body: Column(),
+        body: Column(children: [
+          TipoComidaCard( nombreUsuario: widget.nombreUsuario, tipoDeComida:"Desayuno"),
+          TipoComidaCard( nombreUsuario: widget.nombreUsuario, tipoDeComida:"Almuerzo"),
+          TipoComidaCard( nombreUsuario: widget.nombreUsuario, tipoDeComida:"Comida"),
+          TipoComidaCard( nombreUsuario: widget.nombreUsuario, tipoDeComida:"Merienda"),
+          TipoComidaCard( nombreUsuario: widget.nombreUsuario, tipoDeComida:"Cena"),
+          
+        ],) 
       );
+    
      }
   
 }
